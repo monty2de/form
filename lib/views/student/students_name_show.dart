74bdf52ae9551098c2878d7acef7da27a1ddf45f
monty <over.mind100@gmail.com>
2022-02-20 18:22:15 +0300

    fix error

--- lib/views/student/students_name_show.dart
+++ lib/views/student/students_name_show.dart
@@ -115,6 +115,8 @@ class StudentsNamesShowState extends State<StudentsNamesShow> {
                           newYear = 'الثالثة';
                         } else if (student.year == 'الثالثة') {
                           newYear = 'الرابعة';
+                        }else if (student.year == 'الرابعة') {
+                          newYear = 'متخرج';
                         }
 
                         var subject = FirebaseFirestore.instance
